e66960cd9cfcdec0c2e33f27992eff89a31242e5
J0rd1smit <jordismit20000@gmail.com>
2020-05-25 21:29:03 +0200

    Main dir check

--- zebrafish/utils.py
+++ zebrafish/utils.py
@@ -2,3 +2,7 @@ import os
 
 def get_all_stored_model_paths(models_path = "output"):
     return [os.path.join(models_path, dir) for dir in os.listdir(models_path)]
+
+
+def is_in_main_dir():
+    return os.path.basename(os.path.normpath(os.getcwd())) == "CS4245_cv_project_zebra_fish"
