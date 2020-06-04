54147c8f61510a46ff1b7b11057b10ca82d8ead0
J0rd1smit <jordismit20000@gmail.com>
2020-06-04 14:27:06 +0200

    working data augemntation

--- zebrafish/trainers/TrainingUtil.py
+++ zebrafish/trainers/TrainingUtil.py
@@ -2,10 +2,9 @@ import os
 import pickle
 
 from zebrafish.configs import get_new_output_dir_path
-from zebrafish.trainers.CocoTrainer import CocoTrainer
 
 
-def train(cfg, resume=False):
+def train(cfg, trainer_builder, resume=False):
     if not resume:
         cfg.OUTPUT_DIR = get_new_output_dir_path()
 
@@ -14,6 +13,6 @@ def train(cfg, resume=False):
     with open(cfg.PICKLE, "wb") as f:
         pickle.dump(cfg, f)
 
-    trainer = CocoTrainer(cfg)
+    trainer = trainer_builder(cfg)
     trainer.resume_or_load(resume=resume)
     trainer.train()
